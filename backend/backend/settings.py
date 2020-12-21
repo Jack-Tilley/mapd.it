417537087ff6f3819d654d125ef74a7961c63959
Jack-Tilley <tilley.e.jack@gmail.com>
2020-12-21 16:14:26 -0500

    changed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://mapditbackend.herokuapp.com/']
+ALLOWED_HOSTS = ['mapditbackend.herokuapp.com/']
 
 
 # Application definition
