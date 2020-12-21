11d898b2b0c639eb1d7cf5e1a6591ac89a6ecba8
Jack-Tilley <tilley.e.jack@gmail.com>
2020-12-21 15:12:00 -0500

    changed staticfiles back to staticfiles

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
+STATIC_ROOT = 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
