683ed4b4e07d11c639651f505143a15a830db54b
Jack-Tilley <tilley.e.jack@gmail.com>
2020-12-21 14:45:25 -0500

    heroku setup

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py makemigrations --no-input
+release: python manage.py migrate --no-input
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
+
+django_heroku.settings(locals())
