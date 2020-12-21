62655363d56160bd65b370537806690bb93542ea
Jack-Tilley <tilley.e.jack@gmail.com>
2020-12-21 16:22:04 -0500

    heroku deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'hu9v1bty9c$hs0gdmdec!gjjrkn_-nn9-&z^clt_vis4nzvb6p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mapditbackend.herokuapp.com/']
 
